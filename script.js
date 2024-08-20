c48f3d6e5bb079e276eb17340994ded04aeaa3fb
Surya Yadav <suryayadav222003@gmail.com>
2024-08-20 22:15:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let todoList = [
+  {
+    item: "Surya Free ",
+    dueDate: "4/10/2024",
+  },
+  {
+    item: "Go to office",
+    dueDate: "10/04/2024",
+  },
+];
+
+displayItems();
+
+function addTodo() {
+  let inputElement = document.querySelector("#todo-input");
+  let dateElement = document.querySelector("#todo-date");
+  let todoItem = inputElement.value;
+  let todoDate = dateElement.value;
+  todoList.push({item: todoItem, dueDate: todoDate});
+  inputElement.value = "";
+  dateElement.value = " ";
+
+  displayItems();
+}
+
+function displayItems() {
+  let containerElement = document.querySelector(".todo-container");
+
+  let newHtml = "";
+  for (let i = 0; i < todoList.length; i++) {
+    let item = todoList[i].item;
+    let dueDate = todoList[i].dueDate;
+
+    newHtml += `
+    
+    <span> ${item}</span>
+    <span> ${dueDate}</span>
+        <button class='btn-delete'  onclick = "todoList.splice(${i}, 1);displayItems();">Delete</button>        `;
+  }
+  containerElement.innerHTML = newHtml;
+}
